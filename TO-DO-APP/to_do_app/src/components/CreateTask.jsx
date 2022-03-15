e41e36c2cf7fa11c1b66b8aaf37ac4862a75d5f4
shawasty <shawasty@gmail.com>
2022-03-15 00:47:48 -0400

    fixed a bug in createTask function

--- TO-DO-APP/to_do_app/src/components/CreateTask.jsx
+++ TO-DO-APP/to_do_app/src/components/CreateTask.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 
 export default class CreateTask extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: ''
+        }
+    }
+
+    handleChange=(event)=>{
+        this.setState({task:event.target.value});
+    };
+
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        this.props.createTask(this.state.task);
+        this.setState({task: ''});
+    }
   render() {
     return (
-      <div>CreateTask</div>
+      <form action="" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.task} placeholder='Enter task' onChange={this.handleChange} autoFocus/>
+          <button type='submit'>Add</button>
+      </form>
     )
   }
 }
--- TO-DO-APP/to_do_app/src/components/Main.jsx
+++ TO-DO-APP/to_do_app/src/components/Main.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import CreateTask from './CreateTask';
 import TaskList from './TaskList';
 
+
+const tasks = [];
 export default class Main extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            tasks: [],
+            tasks: tasks,
         };
+    }
+    CreateTask = (task)=>{
+        if(task.trim()==='') {
+            alert("Task can't be empty");
+            return
+        }
+        tasks.push({task,isCompleted:false});
+        this.setState({tasks: tasks});
     }
     render() {
         return (
